e76cb59809c83e9fe7f17a6e28c30fe6cb12d325
Ricardo Kondo <rimkondo@gmail.com>
2023-05-25 16:01:55 -0400

    criando header

--- film/src/App.js
+++ film/src/App.js
@@ -3,10 +3,7 @@ import RoutesApp from "./routes";
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Teste</h1>
-        <RoutesApp />
-      </header>
+      <RoutesApp />
     </div>
   );
 }
