e72e3d991b2fc720d40d24b7bbbc3b033ee1ece8
Ricardo Kondo <rimkondo@gmail.com>
2023-05-26 16:10:35 -0400

    informações do filme

--- film/src/pages/Film/index.js
+++ film/src/pages/Film/index.js
@@ -1,12 +1,65 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import api from "../../services/api";
+import "./filme.css";
 
 function Film() {
   const { id } = useParams();
+  const [film, setFilm] = useState({});
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    async function loadFilm() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "f6a8672f5d18fe891e31254590f7f36e",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          //console.log(response);
+          setFilm(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+        });
+    }
+
+    loadFilm();
+
+    return () => {
+      console.log("Componente foi desmontado");
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes..</h1>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>Acessando Filme {id}</h1>
+    <div className="filme-info">
+      <h1>{film.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${film.backdrop_path}`}
+        alt={film.title}
+      />
+
+      <h3>Sinopse</h3>
+      <span>{film.overview}</span>
+      <strong>Avaliação: {film.vote_average} / 10</strong>
+      {/*
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
+      */}
     </div>
   );
 }
